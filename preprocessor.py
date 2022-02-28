20e1553fcb7c09bbe61fb644682709b20996aa6b
indrakarki <indrakarki1001@gmail.com>
2022-02-28 19:16:35 +0530

    make it better

---
+++ preprocessor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def preprocess(df, region_df):
+    # filtering for summer season
+    df = df[df['Season'] == 'Summer']
+    # merge with region df
+    df = df.merge(region_df, on='NOC', how='left')
+    # dropping duplicates
+    df.drop_duplicates(inplace=True)
+    # one hot encoding
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+    return df
